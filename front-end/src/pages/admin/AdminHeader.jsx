33d3848c21a27393e17f24c8388e9cf9a7310352
Remo Yadav <ry1958112@gmail.com>
2026-01-16 20:06:33 +0545

    notification added in admin

--- front-end/src/pages/admin/AdminHeader.jsx
+++ front-end/src/pages/admin/AdminHeader.jsx
@@ -205,6 +205,7 @@ const Header = () => {
                         </div>
                     </div>
                 </div>
+                <NotificationDrawer />
             </div>
         </div>
     )
